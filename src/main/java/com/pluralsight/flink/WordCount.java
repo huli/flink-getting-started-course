dcd3e3efeae3add8d5c1cd433d0ccfc259b40ac4
Christoph Hilty <christoph.hilty@gmail.com>
2019-09-27 10:40:51 +0200

    Add word count

---
+++ src/main/java/com/pluralsight/flink/WordCount.java
@@ -0,0 +1,31 @@
+package com.pluralsight.flink;
+
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.util.Collector;
+
+public class WordCount {
+
+    public static void main(String[] args) throws Exception {
+
+        final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+        env.socketTextStream("localhost", 9999)
+                .flatMap(new FlatMapFunction<String, Tuple2<String, Integer>>() {
+                    @Override
+                    public void flatMap(String value, Collector<Tuple2<String, Integer>> collector) throws Exception {
+
+                        for(String word: value.split(" ")) {
+
+                            collector.collect(new Tuple2<String, Integer>(word, 1));
+                        }
+                    }
+                })
+                .keyBy(0)
+                .sum(1)
+                .print();
+
+        env.execute("Word count");
+    }
+}
